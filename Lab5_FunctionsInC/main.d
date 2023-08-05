103d2e670851c793409e0bc3b550e4f2140a8bc1
Obscuro <75819639+ProTamLan@users.noreply.github.com>
2023-08-04 19:38:35 -0700

    Add files via upload

---
+++ Lab5_FunctionsInC/main.d
@@ -0,0 +1,4 @@
+.\main.o: main.c
+.\main.o: F:\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\main.o: uart.h
+.\main.o: TExaS.h
---
+++ Lab5_FunctionsInC/uart.d
@@ -0,0 +1,3 @@
+.\uart.o: UART.c
+.\uart.o: UART.h
+.\uart.o: F:\Keil\ARM\ARMCC\bin\..\include\stdio.h
